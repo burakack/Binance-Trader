469c8fd51e2d3d802757102a2c5b083f69a470e5
burakack <burak_ack@hotmail.com>
2022-06-07 18:05:50 +0300

    more trade logic fix

--- TradeStrategies/Bollingertrader.py
+++ TradeStrategies/Bollingertrader.py
@@ -10,17 +10,15 @@ def bollingertrader():
 
     bollinger = BOLLINGER.getBollinger(os.environ['parite'], os.environ['zamandilimi'])
     if os.getenv('didbuy') == "0" and MACDvalue.getMacdValue(os.environ['parite'], os.environ['zamandilimi']) < 0 and \
-            float(Information.getprice(os.environ['parite'])) < bollinger[1][-1] \
-            and RSI.getRsi(os.environ['parite'], os.environ['zamandilimi'])< 50 and \
-            MACD.getMacd(os.environ['parite'], os.environ['zamandilimi'])=="BUY" :
+            float(Information.getprice(os.environ['parite'])) < bollinger[2][-1] \
+            and RSI.getRsi(os.environ['parite'], os.environ['zamandilimi'])< 50:
 
-        print("BOT BUYİNG", os.environ['coin'])
+        print("BOT BUYİNG", os.environ['coin'],"PRICE=",Information.getprice(os.environ['parite']))
         #Tradefuncs.al()
         buyprice = float(Information.getprice(os.environ['parite']))
         while buyprice != 0:
-            Logger.printinfobollinger()
             if os.getenv('didbuy') == "1" and Information.getprice(os.environ['parite']) > bollinger[1][-1] and buyprice\
-                    > float(Information.getprice(os.environ['parite'])) * 1.01:
+                    > float(Information.getprice(os.environ['parite'])):
                 #Tradefuncs.sat()
-                print("BOT SELLİNG", os.environ['coin'])
-                buyprice0 = 0
+                print("BOT SELLİNG", os.environ['coin'],"PRICE=",Information.getprice(os.environ['parite']))
+                buyprice = 0
